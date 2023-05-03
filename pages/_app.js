a8511456539a8241e67169e20f977255f1fc9d3a
Marikka Katrina Højen Jørgensen <mari46re@stud.kea.dk>
2023-05-03 14:35:26 +0200

    SASS install + signin

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 // import "@/styles/globals.css";
 import Layout from "../components/Layout";
+import "../src/styles/main.scss";
 
 export default function App({ Component, pageProps }) {
   console.log("hey jan");
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,50 @@
-import Head from "next/head";
+import { useState } from "react";
 
-export default function Home() {
+export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    // Perform validation here and check credentials with a database or API
+    if (email === "test@test.com" && password === "password") {
+      setLoggedIn(true);
+    }
+  };
+
+  const handleLogout = () => {
+    setLoggedIn(false);
+  };
+
+  if (loggedIn) {
+    return (
+      <div>
+        <h1>You are logged in!</h1>
+        <button onClick={handleLogout}>Logout</button>
+      </div>
+    );
+  }
+
   return (
-    <>
-      {/* <Head>
-        <title>SigniTime</title>
-      </Head> */}
-      <p>hej</p>
-    </>
+    <form onSubmit={handleLogin}>
+      <label>
+        Email
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </label>
+      <label>
+        Password
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      <button type="submit">Login</button>
+    </form>
   );
 }
