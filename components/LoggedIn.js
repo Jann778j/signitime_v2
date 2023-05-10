2b2db5337b08f5833cae2ef2d066c436d95f52a2
Marikka Katrina Højen Jørgensen <mari46re@stud.kea.dk>
2023-05-10 15:25:06 +0200

    Databaseopsætning, the Supabase way

--- components/LoggedIn.js
+++ components/LoggedIn.js
@@ -1,10 +1,78 @@
+import { useEffect, useState } from "react";
+
 export default function LoggedIn(props) {
-  console.log(props.handleLogin);
+  const [clients, setClients] = useState([]);
+  const [projects, setProjects] = useState([]);
+  const [clientProjects, setClientProjects] = useState([]);
+  // const [clientProjects, setClientProjects] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      const { data, error } = await props.supabase.from("signitime-clients")
+        .select(`
+          name
+          `);
+      setClients(data);
+    }
+    getData();
+  }, []);
+
+  useEffect(() => {
+    async function getData() {
+      const { data, error } = await props.supabase.from("signitime-projects")
+        .select(`
+          name
+          `);
+      setProjects(data);
+      // console.log(data);
+    }
+    getData();
+  }, []);
+
+  // console.log(projects);
+  // const displayClientProjects = () => {
+  //   const clientProjects = projects;
+
+  //   clients.forEach((client) => {
+  //     projects.forEach((project) => {
+  //       const exists = client.client_id == project.client_id;
+
+  //       if (exists) {
+  //         clientProjects.name = project.name;
+  //       }
+  //     });
+  //   });
+
+  //   return clientProjects;
+  // };
+
+  // const test = () => {
+  //   console.log(this);
+  // };
+  // console.log(clientProjects);
 
   return (
     <>
       <div>
-        <h1>Hello {props.user.first_name}, You are logged in!</h1>
+        <h1>Hello {props.user.first_name}, you are logged in!</h1>
+        <div className="dropdown">
+          <div className="dropdown-title title">1. Add client</div>
+          {clients.map((client) => (
+            <div
+              className="dropdown-content"
+              // oncClick={setClientProjects(client)}
+              key={client.client_id}
+            >
+              {client.name}
+            </div>
+          ))}
+        </div>
+        <div className="dropdown">
+          <div className="dropdown-title title">2. Add project</div>
+          {projects.map((project) => (
+            <div className="dropdown-content">{project.name}</div>
+          ))}
+        </div>
         {/* <button onClick={handleLogout}>Logout</button> */}
       </div>
     </>
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import SignIn from "@/components/SignIn";
 import LoggedIn from "@/components/LoggedIn";
+import { createClient } from "@supabase/supabase-js";
 
 export default function LoginPage() {
   const [email, setEmail] = useState(""); // State hook til at gemme email-inputtet
@@ -9,30 +10,47 @@ export default function LoginPage() {
   const [password, setPassword] = useState(""); // State hook til at gemme password-inputtet
   const [loggedIn, setLoggedIn] = useState(false); // State hook til at angive, om brugeren er logget ind eller ej
 
-  useEffect(() => {
-    async function getUserData() {
-      const options = {
-        method: "GET",
-        headers: {
-          // API-nøgle til at tilgå brugerdata
-          // Adgangstoken til at autentificere API-anmodninger
-          apikey:
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5wZ3N4Z2doaHZ2c3lnc2h1bXJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMxOTY3ODgsImV4cCI6MTk5ODc3Mjc4OH0.LgnpUXgA5am8PINB41wXA5ffjpBEZeIE1ovMNG5txr8",
-          Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5wZ3N4Z2doaHZ2c3lnc2h1bXJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMxOTY3ODgsImV4cCI6MTk5ODc3Mjc4OH0.LgnpUXgA5am8PINB41wXA5ffjpBEZeIE1ovMNG5txr8",
-        },
-      };
-      // API-endepunktet til at hente brugerdata
-      const res = fetch(
-        "https://npgsxgghhvvsygshumrm.supabase.co/rest/v1/signitime-users",
-        options
-      );
+  const supabaseUrl = "https://npgsxgghhvvsygshumrm.supabase.co/";
+  const supabaseKey =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5wZ3N4Z2doaHZ2c3lnc2h1bXJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMxOTY3ODgsImV4cCI6MTk5ODc3Mjc4OH0.LgnpUXgA5am8PINB41wXA5ffjpBEZeIE1ovMNG5txr8";
+  // const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+  const supabase = createClient(supabaseUrl, supabaseKey);
 
-      const data = await (await res).json(); // Konverterer svaret fra API'en til JSON-format
-      setUsers(data); // Opdaterer brugerdata i state
+  useEffect(() => {
+    async function getData() {
+      const { data, error } = await supabase.from("signitime-users").select(`
+          first_name, last_name, password, initials, email
+          `);
+      setUsers(data);
     }
-    getUserData();
+    getData();
   }, []);
+
+  // console.log(users);
+  // useEffect(() => {
+  //   async function getUserData() {
+  //     const options = {
+  //       method: "GET",
+  //       headers: {
+  //         // API-nøgle til at tilgå brugerdata
+  //         // Adgangstoken til at autentificere API-anmodninger
+  //         apikey:
+  //           "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5wZ3N4Z2doaHZ2c3lnc2h1bXJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMxOTY3ODgsImV4cCI6MTk5ODc3Mjc4OH0.LgnpUXgA5am8PINB41wXA5ffjpBEZeIE1ovMNG5txr8",
+  //         Authorization:
+  //           "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5wZ3N4Z2doaHZ2c3lnc2h1bXJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMxOTY3ODgsImV4cCI6MTk5ODc3Mjc4OH0.LgnpUXgA5am8PINB41wXA5ffjpBEZeIE1ovMNG5txr8",
+  //       },
+  //     };
+  //     // API-endepunktet til at hente brugerdata
+  //     const res = fetch(
+  //       "https://npgsxgghhvvsygshumrm.supabase.co/rest/v1/signitime-users",
+  //       options
+  //     );
+
+  //     const data = await (await res).json(); // Konverterer svaret fra API'en til JSON-format
+  //     setUsers(data); // Opdaterer brugerdata i state
+  //   }
+  //   getUserData();
+  // }, []);
 
   const handleLogin = (e) => {
     e.preventDefault();
@@ -49,7 +67,12 @@ export default function LoginPage() {
   return (
     <>
       {loggedIn ? (
-        <LoggedIn user={user} /> // Hvis brugeren er logget ind, vises komponenten 'LoggedIn'
+        <LoggedIn
+          user={user}
+          supabaseUrl={supabaseUrl}
+          supabaseKey={supabaseKey}
+          supabase={supabase}
+        /> // Hvis brugeren er logget ind, vises komponenten 'LoggedIn'
       ) : (
         <SignIn
           setPassword={setPassword}
